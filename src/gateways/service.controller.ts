8ad6cd5b8081a9a887d2362160069775418bb943
Anthony Lupow <anthony.lupow@gmail.com>
2021-08-26 13:01:12 +0300

    Fix formatting.

--- src/gateways/service.controller.ts
+++ src/gateways/service.controller.ts
@@ -1,4 +1,10 @@
-import { Controller, Get, Query, UsePipes, ValidationPipe } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Query,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { CryptocompareService } from './cryptocompare.service';
 import { PriceRequest } from './price.gateway';
 
